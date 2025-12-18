08eebaaef7bb3fee425583e6dd326f25f803f679
azrul <azrulhafiz3099@gmail.com>
2025-12-19 02:33:49 +0800

    Part Four: Implement IItemManager interface for ItemManager

--- TechnicalAssesmentBackendDeveloper/Program.cs
+++ TechnicalAssesmentBackendDeveloper/Program.cs
@@ -34,24 +34,26 @@
 
         // Part Four (Bonus): Implement an interface IItemManager and make ItemManager implement it.
         // TODO: Implement this part four.
+        IItemManager managerInterface = new ItemManager();
+        managerInterface.AddItem("Orange");
+        managerInterface.PrintAllItems(); // Orange
+        managerInterface.RemoveItem("Orange");
+        managerInterface.PrintAllItems(); // empty
+
+        IItemManager<Fruit> fruitManagerInterface = new ItemManager<Fruit>();
+        fruitManagerInterface.AddItem(new Fruit("Mango"));
+        fruitManagerInterface.PrintAllItems(); // Mango
+
     }
 }
 
-public class ItemManager
+public class ItemManager : IItemManager
 {
     private List<string> items = new List<string>();
 
     public void AddItem(string item)
     {
         items.Add(item);
-    }
-
-    public void PrintAllItems()
-    {
-        foreach (var item in items)
-        {
-            Console.WriteLine(item);
-        }
     }
 
     // Part Two: Implement the RemoveItem method
@@ -69,6 +71,13 @@ public class ItemManager
         }
     }
 
+    public void PrintAllItems()
+    {
+        foreach (var item in items)
+        {
+            Console.WriteLine(item);
+        }
+    }
 
     public void ClearAllItems()
     {
@@ -76,13 +85,26 @@ public class ItemManager
     }
 }
 
-public class ItemManager<T>
+public class ItemManager<T> : IItemManager<T>
 {
     private List<T> items = new List<T>();
 
     public void AddItem(T item)
     {
         items.Add(item);
+    }
+
+    public void RemoveItem(T item)
+    {
+        if (items.Contains(item))
+        {
+            items.Remove(item);
+            Console.WriteLine($"{item} removed.");
+        }
+        else
+        {
+            Console.WriteLine($"{item} not found.");
+        }
     }
 
     public void PrintAllItems()
@@ -99,6 +121,7 @@ public class ItemManager<T>
     }
 }
 
+
 public class Fruit
 {
     public string Name { get; set; }
@@ -112,4 +135,20 @@ public class Fruit
     {
         return Name;
     }
+}
+
+public interface IItemManager
+{
+    void AddItem(string item);
+    void RemoveItem(string item);
+    void PrintAllItems();
+    void ClearAllItems();
+}
+
+public interface IItemManager<T>
+{
+    void AddItem(T item);
+    void RemoveItem(T item);
+    void PrintAllItems();
+    void ClearAllItems();
 }
